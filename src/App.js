d3d0ad4d68f41615c8df946a174797047d86c850
castlehunter <vvxiaobai@gmail.com>
2024-06-08 22:13:56 -0400

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,122 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter />
+    </div>
+  );
+}
+
+function IntroductionParagraph() {
+  return (
+    <div>
+      <h1>Date-Counter</h1>
+      <div className="paragraph">
+        The Date Counter app is a simple tool that displays the date based on a
+        count value that users can adjust. Users can increase or decrease the
+        count using the step value, which determines the increment. The count
+        represents the number of days from today's date. When the count is zero,
+        the app displays today's date. When the count is positive, it shows a
+        future date based on the count. When the count is negative, it shows a
+        past date based on the count. This app provides a straightforward
+        interface for viewing dates relative to today by adjusting the count
+        value.
+      </div>
+    </div>
+  );
+}
+
+function Counter() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+
+  function getDate(num) {
+    const date = new Date();
+    date.setDate(date.getDate() + num);
+    return date.toLocaleDateString("en-US");
+  }
+
+  function handleStepMinus() {
+    setStep((s) => s - 1);
+  }
+
+  function handleStepPlus() {
+    setStep((s) => s + 1);
+  }
+
+  function handleCountMinus() {
+    setCount((c) => c - step);
+  }
+
+  function handleCountPlus() {
+    setCount((c) => c + step);
+  }
+
+  function handleInputField(e) {
+    setCount(Number(e.target.value));
+  }
+
+  function handleReset() {
+    setCount(0);
+    setStep(1);
+  }
+
+  return (
+    <div className="main">
+      <IntroductionParagraph />
+
+      <div>
+        <div>
+          {/* The Slider */}
+          <div>
+            <input
+              type="range"
+              min="0"
+              max="10"
+              value={step}
+              onChange={(e) => setStep(Number(e.target.value))}
+            />
+          </div>
+          <button onClick={handleStepMinus}>-</button>Step: {step}
+          <button onClick={handleStepPlus}>+</button>
+        </div>
+
+        <div>
+          {/* The count + button */}
+          <button onClick={handleCountMinus}>-</button>
+
+          {/* The input field */}
+          <input
+            type="text"
+            placeholder="Item..."
+            value={count}
+            onChange={handleInputField}
+          />
+
+          {/* The count - button */}
+          <button onClick={handleCountPlus}>+</button>
+        </div>
+        {count === 0 && <span>Today is {getDate(0)}</span>}
+        {count > 0 && (
+          <span>
+            {count} day{count > 1 ? "s" : ""} from today is {getDate(count)}
+          </span>
+        )}
+        {count < 0 && (
+          <span>
+            {Math.abs(count)} day{Math.abs(count) > 1 ? "s" : ""} ago was{" "}
+            {getDate(count)}
+          </span>
+        )}
+      </div>
+
+      {count !== 0 || step !== 1 ? (
+        <div>
+          <button onClick={handleReset}>Reset</button>
+        </div>
+      ) : null}
     </div>
   );
 }
